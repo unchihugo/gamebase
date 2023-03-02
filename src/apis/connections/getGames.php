e93809b37ef1c0d180d17e5ef9692b81bb63825e
takko <43926158+takkuchi@users.noreply.github.com>
2023-03-02 23:00:21 +0000

    modified

---
+++ src/apis/connections/getGames.php
@@ -0,0 +1,25 @@
+<?php
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '1234';
+$db_name = 'gamebase_db';
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM game;";
+$result = mysqli_query($conn, $sql);
+
+$games = array();
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $games[] = $row;
+    }
+}
+
+echo json_encode($games);
+
+mysqli_close($conn);
+?>
--- src/apis/gamesApi.php
+++ src/apis/gamesApi.php
@@ -1,27 +1,3 @@
 <?php
-$host="localhost";
-$port=3306;
-$socket="";
-$user="root";
-$password="1234";
-$dbname="gamebase_db";
-
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
-
-//$con->close();
-
-$sql = "SELECT * FROM game;";
-$result = mysqli_query($conn, $sql);
-
-$games = array();
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $games[] = $row;
-    }
-}
-
-echo json_encode($games);
-
-mysqli_close($conn);
+include './connections/getGames.php';
 ?>
